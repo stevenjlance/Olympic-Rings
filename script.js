c12506cde7c0647a7f20f6d234c947ebb4557fc8
Steven Lance <44912347+stevenjlance@users.noreply.github.com>
2022-01-28 15:23:03 +0000

    solutions added

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ function setup() {
 
 function draw() {
   // Create your olympic rings code here!
+	noFill()
+	strokeWeight(5)
+	stroke("blue")
+	ellipse(200,200,100,100)
+	stroke("black")
+	ellipse(300,200,100,100)
+	stroke("red")
+	ellipse(400,200,100,100)
+	stroke("yellow")
+	ellipse(250,250,100,100)
+	stroke("green")
+	ellipse(350,250,100,100)
 	
 	
 }
